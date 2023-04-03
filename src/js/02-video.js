f02edf8d3c33e4077f49d89e5fa56aca600a55a0
MariySemenenko <mashyni92@gmail.com>
2023-04-03 20:11:16 +0300

    task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
 
+//Задача полягає у збереженні часу, на якому користувач зупинив перегляд відео
+
+import Player from '@vimeo/player'; //використовуємо відеоплеєр Vimeo
+ import throttle from 'lodash.throttle';
+
+const videoCurrentTime = 'videoplayer-current-time'; // ключем для сховища буде рядок
+
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    const onPlay = function(data) { //функція onPlay зберігає час в локальному сховищі
+        localStorage.setItem(videoCurrentTime, data.seconds);
+    };
+    
+
+    player.on('play', throttle(onPlay, 1000)); 
+    
+    localStorage.getItem(videoCurrentTime);//Отримуємо збережений час
+    // Якщо значення відсутнє, початковий час- 0
+    player.setCurrentTime(JSON.parse(localStorage.getItem(videoCurrentTime)) || 0);
+        
+    
