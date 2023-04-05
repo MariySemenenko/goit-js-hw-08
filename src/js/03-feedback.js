f65d5e6a38397ea715a4dc1df342660f15251f2e
MariySemenenko <mashyni92@gmail.com>
2023-04-05 22:35:30 +0300

    function reloedPage()

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,14 +3,20 @@ import throttle from 'lodash.throttle';
 const KEY = 'feedback-form-state';//задаю ключ для сховища
 
 const form = document.querySelector('.feedback-form');//отримую форму та її поля
+const { email, message } = form.elements; //сюди потрапляє інформація з атрибутом name
+
+//oтримую данні зі сховища та розпарсюю в обьєкт
+let dataForm = JSON.parse(localStorage.getItem(KEY)) || {};
+
+
+reloedPage();//перезавантаження форми
 
 // додаю обробник події на введення даних користувачем
 form.addEventListener('input', throttle(onInputData, 500));
 form.addEventListener('submit', onFormSubmit);//подія на відправку форми
 
-//oтримую данні зі сховища та розпарсюю в обьєкт
-let dataForm = JSON.parse(localStorage.getItem(KEY)) || {};
-const { email, message } = form.elements; //сюди потрапляє інформація з атрибутом name
+
+
 
 
 function onInputData(e) {//форма для введення користувачем данних
@@ -18,15 +24,19 @@ function onInputData(e) {//форма для введення користува
   localStorage.setItem(KEY, JSON.stringify(dataForm));//приймаємо значення і перетворюємо в рядок
 }
 
+function reloedPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
 
 function onFormSubmit(e) {
   e.preventDefault();//зупиняю перезавантаження
-  if ( email.value === dataForm.email || message.value === dataForm.message) {//якщо форма не пуста
-      console.log({ email: email.value, message: message.value });
-    }
-    else {
-      return alert('fill out the form');
-    }
+    
+      if (email.value === '' || message.value === '') {
+        return alert('Please fill in all the fields!');
+      }
   
 
   localStorage.removeItem(KEY);//видаляю данні
@@ -34,4 +44,5 @@ function onFormSubmit(e) {
   dataForm = {};
 }
 
+////////////////////////////////////////////////////////////
 
