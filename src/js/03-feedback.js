e484a5f1b798bdf533b54e713985ad0d614bb48f
MariySemenenko <mashyni92@gmail.com>
2023-04-06 11:24:49 +0300

    else

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,23 +8,18 @@ const { email, message } = form.elements; //сюди потрапляє інфо
 //oтримую данні зі сховища та розпарсюю в обьєкт
 let dataForm = JSON.parse(localStorage.getItem(KEY)) || {};
 
-
 reloedPage();//перезавантаження форми
 
 // додаю обробник події на введення даних користувачем
 form.addEventListener('input', throttle(onInputData, 500));
 form.addEventListener('submit', onFormSubmit);//подія на відправку форми
 
-
-
-
-
 function onInputData(e) {//форма для введення користувачем данних
   dataForm = { email: email.value, message: message.value };
   localStorage.setItem(KEY, JSON.stringify(dataForm));//приймаємо значення і перетворюємо в рядок
 }
 
-function reloedPage() {
+function reloedPage() {//перезавантаження сторінки
   if (dataForm) {
     email.value = dataForm.email || '';
     message.value = dataForm.message || '';
@@ -33,12 +28,12 @@ function reloedPage() {
 
 function onFormSubmit(e) {
   e.preventDefault();//зупиняю перезавантаження
-    
       if (email.value === '' || message.value === '') {
         return alert('Please fill in all the fields!');
+      } else {
+        console.log({ email: email.value, message: message.value });
       }
   
-
   localStorage.removeItem(KEY);//видаляю данні
   e.currentTarget.reset();
   dataForm = {};
